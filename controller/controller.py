a3b193425f4cfb975ca4ae0ff526191447c2f770
M Nasrul Alawy <alphacsoft@gmail.com>
2020-01-12 15:58:56 +0700

    add redirect sample

--- controller/controller.py
+++ controller/controller.py
@@ -5,3 +5,11 @@ class Index:
     def get(self, req, resp):
         data = {"welcome": "Welcome, Your Web Successfully Running !"}
         resp.text = regius.template("index.html", data=data)
+
+class Name:
+    def get(self, req, resp, nama):
+        resp.text = "Hello " + nama
+
+class SampleRedirect:
+    def get(self, req, resp):
+        regius.redirect(req, resp, "/halo/Jhon")
--- routes/routes.py
+++ routes/routes.py
@@ -2,3 +2,5 @@ from routes import regius
 from controller import controller
 
 regius.add_link('/', controller.Index)
+regius.add_link('/halo/{name}', controller.Name)
+regius.add_link('/redirect', controller.SampleRedirect)
